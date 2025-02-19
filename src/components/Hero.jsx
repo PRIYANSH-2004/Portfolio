ac440e7beea145c37801ebad877594d55eaeab94
PRIYANSH-2004 <110007945+PRIYANSH-2004@users.noreply.github.com>
2025-02-19 14:14:26 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
                          className='my-2 max-w-xl py-6 font-light tracking-tighter'>
                             {HERO_CONTENT}
                         </motion.p>
-                        <a href='https://drive.google.com/file/d/15pKWhrZAjLPbbepWqErZOJu1y1V7M4f_/view?usp=drive_link' target="_blank">
+                        <a href='https://drive.google.com/file/d/1O4EgRx0bYgDhD8HXdB9GG5FPAZepyrt-/view?usp=sharing' target="_blank">
                             <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Resume</button>
 
                         </a>
